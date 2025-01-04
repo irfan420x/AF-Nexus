d66751ccd8226aa36e2a4615fe7fad2244a0427a
AF-Nexus <afnexus01@gmail.com>
2025-01-04 13:59:14 +0100

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,26 @@
+export default {
+  "botName": "Nexus",
+  "prefix": ".",
+  "nickname": "Nexus bot",
+  "adminIds": [
+    "100062160914296",
+    "61557780285734",
+    "100075528791428"
+  ],
+  "facebookAccount": {
+    "email": "astapersonal1@gmail.com",
+    "password": "asta@0123"
+  },
+  "language": "en",
+  "autoRestart": {
+    "time": null
+  },
+  "optionsFca": {
+    "forceLogin": true,
+    "listenEvents": true,
+    "updatePresence": true,
+    "autoMarkDelivery": true,
+    "autoMarkRead": true,
+    "userAgent": "Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Mobile Safari/537.36"
+  }
+}
